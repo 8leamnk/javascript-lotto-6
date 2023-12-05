9c350352c5cf4ce9ff420c723a42439da1cae9d2
8leamnk <silkviewrk@gmail.com>
2023-12-05 19:53:21 +0900

    test: 당첨 번호 유효성 검사 기능 테스트

--- __tests__/domain/LottoTest.js
+++ __tests__/domain/LottoTest.js
@@ -1,18 +1,33 @@
-import Lotto from "../src/Lotto.js";
+import Lotto from '../../src/domain/Lotto';
 
-describe("로또 클래스 테스트", () => {
-  test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
+describe('로또 클래스 테스트', () => {
+  test('로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.', () => {
     expect(() => {
-      new Lotto([1, 2, 3, 4, 5, 6, 7]);
-    }).toThrow("[ERROR]");
+      const lotto = new Lotto([1, 2, 3, 4, 5, 6, 7]);
+      return lotto;
+    }).toThrow('[ERROR]');
   });
 
   // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
-  test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
+  test('로또 번호에 중복된 숫자가 있으면 예외가 발생한다.', () => {
     expect(() => {
-      new Lotto([1, 2, 3, 4, 5, 5]);
-    }).toThrow("[ERROR]");
+      const lotto = new Lotto([1, 2, 3, 4, 5, 5]);
+      return lotto;
+    }).toThrow('[ERROR]');
   });
 
   // 아래에 추가 테스트 작성 가능
+  test('로또 번호에 중복된 숫자가 아닌 값이 있으면 예외가 발생한다.', () => {
+    expect(() => {
+      const lotto = new Lotto([1, 2, 3, 4, 5, NaN]);
+      return lotto;
+    }).toThrow('[ERROR]');
+  });
+
+  test('로또 번호에 1 ~ 45가 아닌 값이 있으면 예외가 발생한다.', () => {
+    expect(() => {
+      const lotto = new Lotto([1, 2, 3, 4, 5, 46]);
+      return lotto;
+    }).toThrow('[ERROR]');
+  });
 });
